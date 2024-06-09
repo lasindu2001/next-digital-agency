c90806444ce82c3d25fbaf477f64357fc53e0d98
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-09 13:11:46 +0530

    create additional files

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Links from './links/Links'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+        <div>Logo</div>
+        <div>
+            <Links />
+        </div>
+    </div>
   )
 }
 
