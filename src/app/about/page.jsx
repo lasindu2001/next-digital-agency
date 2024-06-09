588e2da127d69eab6db4085617bf0d7440dc6ad9
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-09 20:48:07 +0530

    add image assets

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './about.module.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt='About'
+          fill
+        />
+      </div>
+    </div>
   )
 }
 
