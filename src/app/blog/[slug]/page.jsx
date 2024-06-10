b7f38e15e2beac6d4ee2e39efcd172fb53e9b95d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-10 12:50:54 +0530

    create single blog page design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './singleBlog.module.css'
 
 const SingleBlog = () => {
   return (
-    <div>SingleBlog</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/38537/woodland-road-falling-leaf-natural-38537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/38537/woodland-road-falling-leaf-natural-38537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.avatar}
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>
+              Steve Smith
+            </span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              10.06.2024
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>some description</div>
+      </div>
+    </div>
   )
 }
 
