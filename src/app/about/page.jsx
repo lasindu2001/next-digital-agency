5be6b2e0937c0aaccb604bd97f157b02b5fc7c1a
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-10 09:41:43 +0530

    create about page design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,12 +3,39 @@ import styles from './about.module.css'
 
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10+</h1>
+            <p>Years of Experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>234K+</h1>
+            <p>People Reached</p>
+          </div>
+          <div className={styles.box}>
+            <h1>5K+</h1>
+            <p>Services and Plugins</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
         <Image
           src="/about.png"
-          alt='About'
+          alt="About Image"
           fill
+          className={styles.img}
         />
       </div>
     </div>
