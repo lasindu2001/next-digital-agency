7120540a99ec6c118800cb1559455797db378b47
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-20 16:45:47 +0530

    create api routes (delete post)

--- src/app/api/blog/[slug]/route.js
+++ src/app/api/blog/[slug]/route.js
@@ -13,3 +13,15 @@ export const GET = async (req, { params }) => {
         throw new Error("Failed to fetch post!");
     }
 };
+
+export const DELETE = async (req, { params }) => {
+    const { slug } = params;
+    try {
+        connectToDb();
+        await Post.deleteOne({ slug });
+        return NextResponse.json("Post deleted");
+    } catch (err) {
+        console.log(err);
+        throw new Error("Failed to delete post!");
+    }
+};
