b54c514b85dfabab9bf489348c0e7f2bed6c5f89
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-20 18:54:39 +0530

    add github login user to databse

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,13 +1,37 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import { User } from "./models";
+import { connectToDb } from "./utils";
 
-export const { 
-    handlers: { GET, POST }, auth, signIn, signOut 
+export const {
+    handlers: { GET, POST }, auth, signIn, signOut
 } = NextAuth({
-    providers: [ 
-        GitHub ({
+    providers: [
+        GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         })
-    ] 
+    ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            if (account.provider === "github") {
+                connectToDb();
+                try {
+                    const user = await User.findOne({ email: profile.email });
+                    if (!user) {
+                        const newUser = new User({
+                            username: profile.login,
+                            email: profile.email,
+                            image: profile.avatar_url,
+                        });
+                        await newUser.save();
+                    }
+                } catch (err) {
+                    console.log(err);
+                    return false;
+                }
+            }
+            return true;
+        },
+    },
 })
