20bc6535b6a4b931d54576c0c457989545573c6a
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-21 16:13:19 +0530

    complete admin user creation and deletion functionality

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -3,21 +3,29 @@ import styles from "./admin.module.css";
 import AdminPostForm from "@/components/adminPostForm/AdminPostForm";
 import AdminUsers from "@/components/adminUsers/AdminUsers";
 import AdminUserForm from "@/components/adminUserForm/AdminUserForm";
+import { Suspense } from "react";
+import { auth } from "@/lib/auth";
 
-const Admin = () => {
+const Admin = async  () => {
+  const session = await auth();
+
   return (
     <div className={styles.container}>
       <div className={styles.row}>
         <div className={styles.col}>
-          <AdminPosts />
+          <Suspense fallback={<div>Loading...</div>}>
+            <AdminPosts />
+          </Suspense>
         </div>
         <div className={styles.col}>
-          <AdminPostForm />
+          <AdminPostForm userId = {session.user.id}/>
         </div>
       </div>
       <div className={styles.row}>
         <div className={styles.col}>
-          <AdminUsers />
+          <Suspense fallback={<div>Loading...</div>}>
+            <AdminUsers />
+          </Suspense>
         </div>
         <div className={styles.col}>
           <AdminUserForm />
--- src/components/adminUserForm/AdminUserForm.jsx
+++ src/components/adminUserForm/AdminUserForm.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
+
+import { addUser } from "@/lib/action";
+import styles from "./adminUserForm.module.css";
+import { useFormState } from "react-dom";
 
 const AdminUserForm = () => {
+  const [state, formAction] = useFormState(addUser, undefined);
+
   return (
-    <div>AdminUserForm</div>
+    <form action={formAction} className={styles.container}>
+      <h1>Add New User</h1>
+      <input type="text" name="username" placeholder="username" />
+      <input type="text" name="email" placeholder="email" />
+      <input type="password" name="password" placeholder="password" />
+      <input type="text" name="img" placeholder="img" />
+      <select name="isAdmin">
+        <option value="false">Is Admin?</option>
+        <option value="false">No</option>
+        <option value="true">Yes</option>
+      </select>
+      <button>Add</button>
+      {state?.error}
+    </form>
   )
 }
 
